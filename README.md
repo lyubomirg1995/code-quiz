00e663fc1f181eb38470f7db0aa289606c0a586f
lyubomirg1995 <lyubomirg17@gmail.com>
2023-11-22 17:01:11 -0500

    fix: updated hiscores code to properly display scores

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# code-quiz
+## Code Quiz
+
+# Description
+
+The purpose of the activity was to build a multiple-choice quiz with Javascript, which uses Document Object Model (DOM). It is meant to set data in Local Storage and retrieve it to display highscores from past games.
+
+## Usage
+
--- assets/highscores.js
+++ assets/highscores.js
@@ -1,10 +1,12 @@
 const highScoresText = document.querySelector("#high-scores");
 let highScoresTag = '';
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-for (let i = 0; i < localStorage.length; i++) {
-highScoreData = JSON.parse(localStorage.getItem(i+1));
-highScoresTag = highScoresTag.concat('</br><div id="high-scores">'+ highScoreData.initials + " "+"-"+" " + highScoreData.score +'</div>');
-};
+highScores.forEach(highScoreData => {
+    if (highScoreData && highScoreData.initials && highScoreData.score !== undefined) {
+        highScoresTag += `<br><div>${highScoreData.initials} - ${highScoreData.score}</div>`;
+    }
+});
 
 highScoresText.innerHTML = highScoresTag;
